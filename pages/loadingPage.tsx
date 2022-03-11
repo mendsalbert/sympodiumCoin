541afe2a35e03867f9fde43a536115e058e10dea
graphica99 <mendsalbert@gmail.com>
2022-03-11 10:40:32 +0000

    blockchian project frontend ui

--- pages/loadingPage.tsx
+++ pages/loadingPage.tsx
@@ -6,7 +6,7 @@ const LoadingPage = (props: Props) => {
     <>
       <div className="  bg-main-background flex flex-col justify-center items-center w-full h-screen bg-cover bg-center">
         <div className="flex flex-col items-center">
-          <img src="/images/logo.png" className="w-40" />
+          <img src="/images/logo.png" className="w-40 animate-pulse" />
         </div>
       </div>
     </>
