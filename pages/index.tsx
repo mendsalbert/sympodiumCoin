57efc4ced3e4ac20f0c566538efb6317cecd28ec
graphica99 <mendsalbert@gmail.com>
2022-03-11 09:47:31 +0000

    blockchian project frontend ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,28 @@
+import React, { useState } from "react";
 import Link from "next/link";
 import Layout from "../components/Layout";
+import LoadingPage from "./loadingPage";
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-);
+const IndexPage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  setTimeout(() => {
+    setIsLoading(false);
+  }, 500);
+
+  if (isLoading) {
+    return <LoadingPage />;
+  } else {
+    return (
+      <Layout title="Home | Next.js + TypeScript Example">
+        <h1>Hello Next.js 👋</h1>
+        <p>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </p>
+      </Layout>
+    );
+  }
+};
 
 export default IndexPage;
---
+++ pages/loadingPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const LoadingPage = (props: Props) => {
+  return <div>LoadingPage</div>;
+};
+
+export default LoadingPage;
