ad830a3e2e39a6210d93a2266d2cc368db228aab
graphica99 <mendsalbert@gmail.com>
2022-03-11 10:37:27 +0000

    blockchian project frontend ui

--- pages/loadingPage.tsx
+++ pages/loadingPage.tsx
@@ -5,9 +5,8 @@ const LoadingPage = (props: Props) => {
   return (
     <>
       <div className="  bg-main-background flex flex-col justify-center items-center w-full h-screen bg-cover bg-center">
-        <div>
-          <img src="/images/logo.png" className="w-32" />
-          <p>Sympodium</p>
+        <div className="flex flex-col items-center">
+          <img src="/images/logo.png" className="w-40" />
         </div>
       </div>
     </>
