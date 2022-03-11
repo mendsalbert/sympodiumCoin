c7c0e7d0e4907dfe7b8872e453cf46a5279553b0
graphica99 <mendsalbert@gmail.com>
2022-03-11 16:10:28 +0000

    front end ui for the blockchain

--- pages/Homepage.tsx
+++ pages/Homepage.tsx
@@ -38,11 +38,11 @@ const Homepage = (props: Props) => {
           src="/images/circle2.png"
           className="w-64 absolute top-12 ml-32 left-96 -z-0"
         />
-        <div className="flex flex-row  justify-between space-x-20 my-32">
+        <div className="grid grid-cols-3 gap-24 my-32">
           <div className=" text-center text-white font-bold p-6 flex flex-col justify-between  relative  bg-gradient-to-r from-[#ffffff8c] to-[#fff7007a] shadow-lg  bg-clip-padding bg-opacity-30 rounded-xl   backdrop-filter: blur(20px)">
             <div className="text-center  mb-7 justify-between ">
               <p className="uppercase text-7xl font-bold py-4 ">
-                <CountUp delay={2} end={300} />
+                <CountUp delay={2} end={300} />+
               </p>
             </div>
             <div className="space-y-0">
@@ -58,7 +58,7 @@ const Homepage = (props: Props) => {
               </p>
             </div>
             <div className="space-y-0">
-              <p className="text-xl">TRANSACTIONS MADE</p>
+              <p className="text-xl">BLOCKS MINED</p>
             </div>
           </div>
 
@@ -70,7 +70,7 @@ const Homepage = (props: Props) => {
               </p>
             </div>
             <div className="space-y-0">
-              <p className="text-xl">TRANSACTIONS MADE</p>
+              <p className="text-xl">WALLETS</p>
             </div>
           </div>
         </div>
