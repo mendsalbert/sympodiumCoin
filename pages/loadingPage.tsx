df2dfcc9be0b29350bcddf0e5548d7f6dcf4ff5b
graphica99 <mendsalbert@gmail.com>
2022-03-11 10:17:22 +0000

    blockchian project frontend ui

--- pages/loadingPage.tsx
+++ pages/loadingPage.tsx
@@ -6,9 +6,8 @@ const LoadingPage = (props: Props) => {
   return (
     <>
       <div className=" relative bg-main-background w-full h-screen bg-cover bg-center">
-        <div className="fixed left-0 right-0  w-full h-screen bg-[#0a7abf62]"></div>
+        <div>Welcome</div>
       </div>
-      <div className="z-auto text-white">LoadingPage</div>
     </>
   );
 };
