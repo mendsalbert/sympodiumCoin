fcfb1b6a3e000cd9b0b90732052894b3b8c0d0b2
graphica99 <mendsalbert@gmail.com>
2022-03-11 13:06:32 +0000

    blockchian project frontend ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,19 @@ const IndexPage = () => {
   } else {
     return (
       <Layout title="Home | Next.js + TypeScript Example">
-        <div></div>
+        <div className="flex flex-row justify-between items-start mt-20">
+          <div className="flex flex-col space-y-4 text-white ">
+            <p className="font-bold text-6xl ">WC3</p>
+            <p className="text-xl">
+              WeCoin 3. World Frist Community Based Cryptocurrency.
+            </p>
+            <div className="rounded-full text-center py-2 px-5 text-lg w-6/12 bg-[#5F6FFB]">
+              Get Started
+            </div>
+          </div>
+
+          <div></div>
+        </div>
       </Layout>
     );
   }
