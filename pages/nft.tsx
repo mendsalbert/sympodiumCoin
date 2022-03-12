9ce79c9760bbde671bed9b0bd5dfd2ca7349f6f0
graphica99 <mendsalbert@gmail.com>
2022-03-12 02:12:06 +0000

    front end ui for the blockchain

---
+++ pages/nft.tsx
@@ -0,0 +1,67 @@
+import {
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  ClockIcon,
+  CreditCardIcon,
+  CubeTransparentIcon,
+  HashtagIcon,
+} from "@heroicons/react/outline";
+import React from "react";
+import CountUp from "react-countup";
+import Layout from "../components/Layout";
+type Props = {};
+
+const Blocks = (props: Props) => {
+  return (
+    <Layout>
+      <div className="my-16">
+        <div className="  flex flex-col lg:flex-row items-center justify-center space-x-4">
+          <img src="/images/nft.png" className="w-32" />
+          <p className="font-extrabold flex flex-row space-x-2 text-center text-white text-5xl  lg:text-6xl ">
+            NFT
+          </p>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-3  lg:grid-cols-4  my-12 gap-4 lg:gap-8"></div>
+        <div>
+          <div className="max-w-8xl mx-auto container py-10">
+            <ul className="flex justify-center items-center">
+              <li>
+                <ChevronLeftIcon className="h-6 text-white" />
+              </li>
+              <li>
+                <span className=" focus:bg-indigo-700 focus:text-white flex text-indigo-700 bg-white hover:bg-indigo-600 hover:text-white text-base leading-tight font-bold cursor-pointer shadow transition duration-150 ease-in-out mx-2 sm:mx-4 rounded px-3 py-2 ">
+                  1
+                </span>
+              </li>
+              <li>
+                <span className=" focus:bg-indigo-700 focus:text-white flex text-indigo-700 bg-white hover:bg-indigo-600 hover:text-white text-base leading-tight font-bold cursor-pointer shadow transition duration-150 ease-in-out mx-2 sm:mx-4 rounded px-3 py-2 ">
+                  2
+                </span>
+              </li>
+              <li>
+                <span className=" focus:bg-indigo-700 focus:text-white flex text-indigo-700 bg-white hover:bg-indigo-600 hover:text-white rounded transition duration-150 ease-in-out text-base leading-tight font-bold shadow px-3 py-2 ">
+                  ...
+                </span>
+              </li>
+              <li>
+                <span className=" focus:bg-indigo-700 focus:text-white flex text-indigo-700 bg-white hover:bg-indigo-600 hover:text-white text-base leading-tight font-bold cursor-pointer transition shadow duration-150 ease-in-out mx-2 sm:mx-4 rounded px-3 py-2 ">
+                  6
+                </span>
+              </li>
+              <li>
+                <span className=" focus:bg-indigo-700 focus:text-white flex text-indigo-700 bg-white hover:bg-indigo-600 hover:text-white text-base leading-tight font-bold cursor-pointer transition shadow duration-150 ease-in-out mx-2 sm:mx-4 rounded px-3 py-2 focus:outline-none">
+                  7
+                </span>
+              </li>
+              <li>
+                <ChevronRightIcon className="h-7 text-white" />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Blocks;
