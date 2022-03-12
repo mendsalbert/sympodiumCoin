55279b8a8ccd5eca540ab3d455f678b17df0d161
graphica99 <mendsalbert@gmail.com>
2022-03-12 01:32:54 +0000

    front end ui for the blockchain

--- pages/wallet.tsx
+++ pages/wallet.tsx
@@ -11,18 +11,18 @@ type Props = {};
 const Wallet = (props: Props) => {
   return (
     <div className="flex flex-col lg:flex-row justify-between w-full items-start lg:space-x-10">
-      <div className=" my-12 lg:my-14 w-full ">
+      <div className=" mt-12 mb-5 lg:mb-0 lg:my-14 w-full ">
         <div className=" text-black font-bold  p-6 flex flex-col justify-between  relative bg-white shadow-lg  bg-clip-padding bg-opacity-30 rounded-xl border border-gray-200 bg-gradient-to-r from-[#ffcc33] via-[#ffd700] to-[#e1ad21]  backdrop-filter: blur(20px)">
           <div className="flex flex-row items-center mb-5 lg:mb-14 justify-between ">
             <img src="/images/cardlogo.png" className="w-16" />
             <p className="uppercase text-lg">Sympodium</p>
           </div>
           <div className="flex flex-row justify-between items-center space-y-4">
-            <div>
-              <p className="text-xl ">0aB3 XXXX XXXX XXXX XXXX</p>
-              <p className="text-xl">MENDS ALBERT</p>
+            <div className="sm:w-6/12">
+              <p className="text-md lg:text-xl">0aB3 XXXX XXXX XXXX XXXX</p>
+              <p className="text-md lg:text-xl">MENDS ALBERT</p>
             </div>
-            <p className="text-3xl">1000 SC</p>
+            <p className="text-xl lg:text-3xl ">1000 SC</p>
           </div>
         </div>
         <div className=" flex space-x-5 my-5 flex-row items-center justify-between">
@@ -34,7 +34,7 @@ const Wallet = (props: Props) => {
           </div>
         </div>
       </div>
-      <div className="my-8 lg:my-14 text-white w-full">
+      <div className="my-8  lg:my-14 text-white w-full">
         <p className="text-2xl mb-4">Transactions (3)</p>
 
         <div className="w-full space-y-4">
