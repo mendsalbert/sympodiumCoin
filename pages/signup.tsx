63b56bea1eb52908f30b5d45565d67f05d14a601
graphica99 <mendsalbert@gmail.com>
2022-03-12 10:40:43 +0000

    front end ui for the blockchain

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -6,18 +6,18 @@ type Props = {};
 const Signup = (props: Props) => {
   return (
     <Layout>
-      {/* <div className="font-Montserrat  flex flex-col justify-center items-center dark:bg-main-background-dark bg-main-background bg-cover overflow-x-hidden  bg-no-repeat  bg-center h-screen"> */}
-      <div className=" text-white font-bold p-6 flex flex-col justify-between w-full lg:w-7/12 relative bg-white shadow-lg  bg-clip-padding bg-opacity-30 rounded-xl border border-gray-200 backdrop-filter: blur(20px)">
-        <div className="flex flex-row items-center mb-5 lg:mb-14 justify-between ">
-          <img src="/images/cardlogo.png" className="w-16" />
-          <p className="uppercase text-lg">Sympodium</p>
-        </div>
-        <div className="space-y-4">
-          <p className="text-xl ">0aB3 XXXX XXXX XXXX XXXX</p>
-          <p className="text-xl">SYMPODIUM COIN</p>
+      <div className="  flex flex-col justify-center items-center ">
+        <div className=" text-white font-bold p-6 flex flex-col justify-between w-full lg:w-7/12 relative bg-white shadow-lg  bg-clip-padding bg-opacity-30 rounded-xl backdrop-filter: blur(20px)">
+          <div className="flex flex-row items-center mb-5 lg:mb-14 justify-between ">
+            <img src="/images/cardlogo.png" className="w-16" />
+            <p className="uppercase text-lg">Sympodium</p>
+          </div>
+          <div className="space-y-4">
+            <p className="text-xl ">0aB3 XXXX XXXX XXXX XXXX</p>
+            <p className="text-xl">SYMPODIUM COIN</p>
+          </div>
         </div>
       </div>
-      {/* </div> */}
     </Layout>
   );
 };
