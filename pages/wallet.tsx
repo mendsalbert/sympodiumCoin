b60b70579fa8f70e746167c174b79fe51aba6a20
graphica99 <mendsalbert@gmail.com>
2022-03-12 01:29:31 +0000

    front end ui for the blockchain

--- pages/wallet.tsx
+++ pages/wallet.tsx
@@ -11,7 +11,7 @@ type Props = {};
 const Wallet = (props: Props) => {
   return (
     <div className="flex flex-col lg:flex-row justify-between w-full items-start lg:space-x-10">
-      <div className=" my-10 lg:my-14 w-full ">
+      <div className=" my-12 lg:my-14 w-full ">
         <div className=" text-black font-bold  p-6 flex flex-col justify-between  relative bg-white shadow-lg  bg-clip-padding bg-opacity-30 rounded-xl border border-gray-200 bg-gradient-to-r from-[#ffcc33] via-[#ffd700] to-[#e1ad21]  backdrop-filter: blur(20px)">
           <div className="flex flex-row items-center mb-5 lg:mb-14 justify-between ">
             <img src="/images/cardlogo.png" className="w-16" />
@@ -34,7 +34,7 @@ const Wallet = (props: Props) => {
           </div>
         </div>
       </div>
-      <div className="my-14 text-white w-full   ">
+      <div className="my-8 lg:my-14 text-white w-full">
         <p className="text-2xl mb-4">Transactions (3)</p>
 
         <div className="w-full space-y-4">
