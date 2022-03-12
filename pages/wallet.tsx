983688a2b3481838143ddf7d6b7cc04c9cb846f9
graphica99 <mendsalbert@gmail.com>
2022-03-12 01:06:54 +0000

    front end ui for the blockchain

--- pages/wallet.tsx
+++ pages/wallet.tsx
@@ -33,14 +33,26 @@ const Wallet = (props: Props) => {
         <p>Transactions (2)</p>
 
         <div className="w-full">
-          <div className="flex flex-row space-x-6 ">
-            <div className="bg-white rounded-md p-2">
-              <TicketIcon className="h-7 text-black" />
-            </div>
-            <div>
-              <p>To: </p>
-              <p>Amout:</p>
-              <p>Date:</p>
+          <div className="h-80 py-8">
+            <div className="mx-auto flex justify-center sm:justify-end h-full">
+              <div
+                role="alert"
+                className="sm:mr-6 xl:w-5/12 mx-auto absolute left-0 sm:left-auto right-0 sm:w-6/12 md:w-3/5 justify-between w-11/12 bg-white dark:bg-gray-800 shadow-lg rounded flex sm:flex-row flex-col transition duration-150 ease-in-out"
+                id="notification"
+              >
+                <div className="sm:px-6 p-2 flex mt-4 sm:mt-0 ml-4 sm:ml-0 items-center justify-center bg-green-700 sm:rounded-tl sm:rounded-bl w-12 h-12 sm:h-auto sm:w-auto text-white">
+                  <TicketIcon className="h-7 text-white" />
+                </div>
+                <div className="flex flex-col justify-center xl:-ml-4 pl-4 xl:pl-1 sm:w-3/5 pt-4 sm:pb-4 pb-2">
+                  <h1 className="text-lg text-gray-800 dark:text-gray-100 font-semibold pb-1">
+                    Action Completed
+                  </h1>
+                  <p className="text-sm text-gray-600 dark:text-gray-400 font-normal">
+                    You have successfully completed the survey. You will soon
+                    receive a reward email. Stay tuned.
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
