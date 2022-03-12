2e167d89ff5ac7e16d2f13e9a758dd39f7cc6370
graphica99 <mendsalbert@gmail.com>
2022-03-12 00:53:15 +0000

    front end ui for the blockchain

--- pages/wallet.tsx
+++ pages/wallet.tsx
@@ -4,7 +4,7 @@ type Props = {};
 
 const Wallet = (props: Props) => {
   return (
-    <div>
+    <div className="flex flex-row justify-between items-start">
       <div className=" my-14 flex flex-col justify-center items-center">
         <div className=" text-black font-bold p-6 flex flex-col justify-between w-6/12 relative bg-white shadow-lg  bg-clip-padding bg-opacity-30 rounded-xl border border-gray-200 bg-gradient-to-r from-[#ffcc33] via-[#ffd700] to-[#e1ad21]  backdrop-filter: blur(20px)">
           <div className="flex flex-row items-center mb-5 lg:mb-14 justify-between ">
@@ -27,6 +27,9 @@ const Wallet = (props: Props) => {
             WITHDRAW
           </div>
         </div>
+      </div>
+      <div>
+        <p>Transactions</p>
       </div>
     </div>
   );
