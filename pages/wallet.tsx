76e642e3c217922a2976fc08e95a34e8ded254fa
graphica99 <mendsalbert@gmail.com>
2022-03-12 00:49:13 +0000

    front end ui for the blockchain

--- pages/wallet.tsx
+++ pages/wallet.tsx
@@ -20,10 +20,10 @@ const Wallet = (props: Props) => {
           </div>
         </div>
         <div className="w-6/12 flex space-x-5 my-5 flex-row items-center justify-between">
-          <div className="uppercase  rounded-md text-center py-2 px-5 text-white text-lg w-full cursor-default bg-gradient-to-r from-pink-500 to-yellow-500 ">
+          <div className="uppercase  rounded-md text-center py-2 px-5 text-white text-lg w-full cursor-default bg-gradient-to-r from-blue-500 to-blue-900 ">
             TRANSFER
           </div>
-          <div className="uppercase  rounded-md text-center py-2 px-5 text-white text-lg w-full cursor-default bg-gradient-to-r from-pink-500 to-yellow-500 ">
+          <div className="uppercase  rounded-md text-center py-2 px-5 text-white text-lg w-full cursor-default bg-gradient-to-r from-blue-600 to-pink-600 ">
             WITHDRAW
           </div>
         </div>
