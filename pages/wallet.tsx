878f260ab508db7a686c36eb0fd055845d77754c
graphica99 <mendsalbert@gmail.com>
2022-03-12 00:45:51 +0000

    front end ui for the blockchain

--- pages/wallet.tsx
+++ pages/wallet.tsx
@@ -5,7 +5,7 @@ type Props = {};
 const Wallet = (props: Props) => {
   return (
     <div>
-      <div className=" my-9 flex flex-row justify-center items-center">
+      <div className=" my-9 flex flex-col justify-center items-center">
         <div className=" text-black font-bold p-6 flex flex-col justify-between w-6/12 relative bg-white shadow-lg  bg-clip-padding bg-opacity-30 rounded-xl border border-gray-200 bg-gradient-to-r from-[#ffcc33] via-[#ffd700] to-[#e1ad21]  backdrop-filter: blur(20px)">
           <div className="flex flex-row items-center mb-5 lg:mb-14 justify-between ">
             <img src="/images/cardlogo.png" className="w-16" />
@@ -19,9 +19,17 @@ const Wallet = (props: Props) => {
             <p className="text-3xl">1000 SC</p>
           </div>
         </div>
+        <div className="w-6/12 flex  flex-row items-center justify-between">
+          <div className="uppercase  rounded-full text-center py-2 px-5 text-white text-lg w-full cursor-default bg-gradient-to-r from-pink-500 to-yellow-500 ">
+            TRANSFER
+          </div>
+          <div className="uppercase  rounded-full text-center py-2 px-5 text-white text-lg w-full cursor-default bg-gradient-to-r from-pink-500 to-yellow-500 ">
+            WITHDRAW
+          </div>
+        </div>
       </div>
 
-      <div className="w-6/12 flex flex-row items-center justify-between">
+      <div className="w-6/12 flex  flex-row items-center justify-between">
         <div className="uppercase  rounded-full text-center py-2 px-5 text-white text-lg w-full cursor-default bg-gradient-to-r from-pink-500 to-yellow-500 ">
           TRANSFER
         </div>
