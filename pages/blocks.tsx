bdbecaae2b26a7210b818a2d608ce7f03dec4062
graphica99 <mendsalbert@gmail.com>
2022-03-12 00:00:05 +0000

    front end ui for the blockchain

--- pages/blocks.tsx
+++ pages/blocks.tsx
@@ -4,18 +4,17 @@ type Props = {};
 
 const Blocks = (props: Props) => {
   return (
-    <div className="my-32">
+    <div className="my-16">
       <div className="flex flex-row items-center justify-center space-x-4">
-        <p className="font-extrabold  text-center text-white text-5xl  lg:text-6xl ">
+        <p className="font-extrabold flex flex-row space-x-2 text-center text-white text-5xl  lg:text-6xl ">
+          <p className="font-extrabold  text-white md:text-transparent  text-[2.5rem] md:text-5xl w-full lg:text-6xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-400">
+            <CountUp delay={1} end={150} />+
+          </p>
           Blocks
         </p>
         <img src="/images/cube.png" className="w-32" />
       </div>
-      <div className="text-center">
-        <p className="font-bold  text-white md:text-transparent  text-[2.5rem] md:text-5xl w-full lg:text-6xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-400">
-          <CountUp delay={1} end={150} />+
-        </p>
-      </div>
+      <div className="text-center"></div>
     </div>
   );
 };
