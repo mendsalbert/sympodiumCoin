bcc2dfd8e1c35afa3ad93030d46bfd05592cefd0
graphica99 <mendsalbert@gmail.com>
2022-03-12 01:25:46 +0000

    front end ui for the blockchain

--- pages/wallet.tsx
+++ pages/wallet.tsx
@@ -12,7 +12,7 @@ const Wallet = (props: Props) => {
   return (
     <div className="flex flex-row justify-between w-full items-start space-x-10">
       <div className=" my-14 w-full ">
-        <div className=" text-black font-bold p-6 flex flex-col justify-between  relative bg-white shadow-lg  bg-clip-padding bg-opacity-30 rounded-xl border border-gray-200 bg-gradient-to-r from-[#ffcc33] via-[#ffd700] to-[#e1ad21]  backdrop-filter: blur(20px)">
+        <div className=" text-black font-bold  p-6 flex flex-col justify-between  relative bg-white shadow-lg  bg-clip-padding bg-opacity-30 rounded-xl border border-gray-200 bg-gradient-to-r from-[#ffcc33] via-[#ffd700] to-[#e1ad21]  backdrop-filter: blur(20px)">
           <div className="flex flex-row items-center mb-5 lg:mb-14 justify-between ">
             <img src="/images/cardlogo.png" className="w-16" />
             <p className="uppercase text-lg">Sympodium</p>
@@ -35,9 +35,43 @@ const Wallet = (props: Props) => {
         </div>
       </div>
       <div className="my-14 text-white w-full   ">
-        <p className="text-2xl mb-4">Transactions (2)</p>
+        <p className="text-2xl mb-4">Transactions (3)</p>
 
-        <div className="w-full">
+        <div className="w-full space-y-4">
+          <div className="w-full p-2 bg-white dark:bg-gray-600 rounded flex">
+            {/* <TicketIcon className="h-8 text-gray-700" /> */}
+            <div className="pl-3  space-y-0">
+              <p className="dark:text-gray-300 focus:outline-none text-md flex flex-row items-center space-x-3 leading-3 pt-1 text-gray-500">
+                <UserIcon className="h-6 text-gray-700 dark:text-gray-400" />
+                0x5ffafd54fadf56afeh65665
+              </p>
+              <p className="dark:text-gray-300 focus:outline-none text-md flex flex-row items-center space-x-3 leading-3 pt-1 text-gray-500">
+                <CashIcon className="h-6 text-gray-700 dark:text-gray-400" /> 50
+                SC
+              </p>
+              <p className="dark:text-gray-300 focus:outline-none text-md flex flex-row items-center space-x-3 leading-3 pt-1 text-gray-500">
+                <ClockIcon className="h-6 text-gray-700 dark:text-gray-400" />
+                Date
+              </p>
+            </div>
+          </div>
+          <div className="w-full p-2 bg-white dark:bg-gray-600 rounded flex">
+            {/* <TicketIcon className="h-8 text-gray-700" /> */}
+            <div className="pl-3  space-y-0">
+              <p className="dark:text-gray-300 focus:outline-none text-md flex flex-row items-center space-x-3 leading-3 pt-1 text-gray-500">
+                <UserIcon className="h-6 text-gray-700 dark:text-gray-400" />
+                0x5ffafd54fadf56afeh65665
+              </p>
+              <p className="dark:text-gray-300 focus:outline-none text-md flex flex-row items-center space-x-3 leading-3 pt-1 text-gray-500">
+                <CashIcon className="h-6 text-gray-700 dark:text-gray-400" /> 50
+                SC
+              </p>
+              <p className="dark:text-gray-300 focus:outline-none text-md flex flex-row items-center space-x-3 leading-3 pt-1 text-gray-500">
+                <ClockIcon className="h-6 text-gray-700 dark:text-gray-400" />
+                Date
+              </p>
+            </div>
+          </div>
           <div className="w-full p-2 bg-white dark:bg-gray-600 rounded flex">
             {/* <TicketIcon className="h-8 text-gray-700" /> */}
             <div className="pl-3  space-y-0">
