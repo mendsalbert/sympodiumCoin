624b0a29d228b678469c603ba7647a226057fd53
graphica99 <mendsalbert@gmail.com>
2022-03-12 10:37:58 +0000

    front end ui for the blockchain

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -3,7 +3,9 @@ import React from "react";
 type Props = {};
 
 const Signup = (props: Props) => {
-  return <div>signup</div>;
+  return (
+    <div className="font-Montserrat  flex flex-col justify-center items-center dark:bg-main-background-dark bg-main-background bg-cover overflow-x-hidden  bg-no-repeat  bg-center h-screen"></div>
+  );
 };
 
 export default Signup;
