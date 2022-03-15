52643c61b1d1bf75b68095544c1ae7f12ca8d198
graphica99 <mendsalbert@gmail.com>
2022-03-15 05:54:33 +0000

    front end ui for the blockchain

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,7 +14,8 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;600&family=League+Gothic&family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Nunito+Sans:wght@200;300;400;600;700&family=Secular+One&display=swap"
           rel="stylesheet"
         />
-        <link rel="shortcut icon" href="/static/favicon.png" />
+
+        <link rel="icon" type="image/png" href="/images/logo.png" />
       </Head>
       <body>
         <Main />
